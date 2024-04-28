9aff93e859cb0c58aa65dae0463a617586513ce9
Yegor Bugayenko <yegor256@gmail.com>
2024-04-28 09:00:38 +0300

    year up

--- lib/threads.rb
+++ lib/threads.rb
@@ -2,7 +2,7 @@
 
 # (The MIT License)
 #
-# Copyright (c) 2018-2023 Yegor Bugayenko
+# Copyright (c) 2018-2024 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -27,7 +27,7 @@ require 'backtrace'
 
 # Threads.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2018-2023 Yegor Bugayenko
+# Copyright:: Copyright (c) 2018-2024 Yegor Bugayenko
 # License:: MIT
 class Threads
   def initialize(total = Concurrent.processor_count * 8, log: $stdout)
